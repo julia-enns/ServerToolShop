d0c96cdec35619b9548005cd0a27a6f522e7ec6c
kacper <kporebski15@gmail.com>
2019-04-02 14:04:39 -0600

    guifinished

--- MainFrame.java
+++ MainFrame.java
@@ -12,15 +12,17 @@ public class MainFrame extends JFrame {
 
     private JButton listButton = new JButton("List All");
 
-    private JButton toolNameButton  = new JButton("Tool Name");
+    private JButton toolNameButton = new JButton("Tool Name");
 
-    private JButton toolIDButton  = new JButton("Tool ID");
+    private JButton toolIDButton = new JButton("Tool ID");
 
     private JButton quantityButton = new JButton("Quantity");
 
     private JButton printOrderButton = new JButton("Print Order");
 
-    private DefaultListModel<String> listModel;
+    private JButton buyButton = new JButton("Buy Item");
+
+    private DefaultListModel<String> toolList;
 
     private JList<String> listArea;
 
@@ -30,7 +32,7 @@ public class MainFrame extends JFrame {
      * Constructs the main gui window.
      */
 
-    public MainFrame(){
+    public MainFrame() {
         this.setTitle("Main Window");
         this.setSize(700, 400);
         this.add(createButtonPanel(), BorderLayout.WEST);
@@ -38,11 +40,13 @@ public class MainFrame extends JFrame {
         this.add(createUpperPanel(), BorderLayout.NORTH);
         this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
     }
+
     /**
      * Creates the a panel with required buttons.
+     *
      * @return the button panel.
      */
-    private JPanel createButtonPanel(){
+    private JPanel createButtonPanel() {
         JPanel buttonPanel = new JPanel(new GridLayout(0, 1, 100, 10));
 
         buttonPanel.add(listButton);
@@ -50,17 +54,19 @@ public class MainFrame extends JFrame {
         buttonPanel.add(toolIDButton);
         buttonPanel.add(quantityButton);
         buttonPanel.add(printOrderButton);
+        buttonPanel.add(buyButton);
         return buttonPanel;
     }
 
     /**
      * Creates the center panel with required list.
+     *
      * @return the center panel.
      */
 
     private JScrollPane createCenterPanel() {
-        listModel = new DefaultListModel<String>();
-        listArea = new JList<String>(listModel);
+        toolList = new DefaultListModel<String>();
+        listArea = new JList<String>(toolList);
         String width = "1234567890123456789012345678901234567890";
         listArea.setPrototypeCellValue(width);
         listArea.setFont(new Font("Courier New", Font.BOLD, 12));
@@ -71,6 +77,7 @@ public class MainFrame extends JFrame {
 
     /**
      * Creates upper panel with a label describing the program.
+     *
      * @return the upper panel.
      */
     private JPanel createUpperPanel() {
@@ -82,33 +89,42 @@ public class MainFrame extends JFrame {
 
     /**
      * Conncets a listener to the list button
+     *
      * @param listen listener to connect.
      */
-    public void addListListener(ActionListener listen){
+    public void addListListener(ActionListener listen) {
         listButton.addActionListener(listen);
     }
+
     /**
      * Conncets a listener to the toolName button
+     *
      * @param listen listener to connect.
      */
-    public void addToolNameListener(ActionListener listen){ toolNameButton.addActionListener(listen);
-}
+    public void addToolNameListener(ActionListener listen) {
+        toolNameButton.addActionListener(listen);
+    }
+
     /**
      * Conncets a listener to the toolID button
+     *
      * @param listen listener to connect.
      */
-    public void addToolIDListener(ActionListener listen){
+    public void addToolIDListener(ActionListener listen) {
         toolIDButton.addActionListener(listen);
-}
+    }
+
     /**
      * Conncets a listener to the quantity button
+     *
      * @param listen listener to connect.
      */
-    public void addQuantityListener(ActionListener listen){
+    public void addQuantityListener(ActionListener listen) {
         quantityButton.addActionListener(listen);
-}
+    }
 
-    public void addOrderListener(ActionListener listen){ printOrderButton.addActionListener(listen);
+    public void addOrderListener(ActionListener listen) {
+        printOrderButton.addActionListener(listen);
     }
 
     public JButton getListButton() {
@@ -131,8 +147,7 @@ public class MainFrame extends JFrame {
         return toolNameButton;
     }
 
-
-    public DefaultListModel<String> getListModel() {
-        return listModel;
+    public DefaultListModel<String> getToolList() {
+        return toolList;
     }
 }
